5f8c92ccc712b109fbbc6b0dfe2efc58c213ed93
Aron Snekkestad <aron@snekkestad.com>
2024-03-23 17:20:47 +0100

    feat: import component in root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
+import { CommentInput } from './CommentInput/CommentInput';
 import { CommentList } from './components/CommentList/CommentList';
 import data from './data.json';
 
 function App() {
   return (
     <div className="comment-section">
-      <CommentList comments={data.comments} />
+      <CommentList comments={data.comments} currentUserImage={data.currentUser.image.png} />
+      <CommentInput currentUserImage={data.currentUser.image.png} placeholder={'SEND'} />
     </div>
   );
 }
