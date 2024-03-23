57c3ee6c91d6f771c2ca1e5fcda4f5f21af4684b
Aron Snekkestad <aron@snekkestad.com>
2024-03-23 17:18:05 +0100

    feat: extend component with onClick prop

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Button.css';
 
-export function Button({ className, iconSvg, placeholder, type, path }) {
+export function Button({ className, iconSvg, placeholder, onClick }) {
   return (
-    <button className={`button ${className ?? ''}`}>
+    <button onClick={onClick} className={`button ${className ?? ''}`}>
       {iconSvg}
       {placeholder}
     </button>
