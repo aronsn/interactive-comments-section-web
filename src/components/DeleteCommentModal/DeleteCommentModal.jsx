4ffeb2eb2c4adb509931bc59ede50ecf2d7384ec
Aron Snekkestad <aron@snekkestad.com>
2024-03-23 22:23:23 +0100

    feat: design modal component

--- src/components/DeleteCommentModal/DeleteCommentModal.jsx
+++ src/components/DeleteCommentModal/DeleteCommentModal.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import './DeleteCommentModal.css';
+import { Button } from '../Button/Button';
 
-export const DeleteCommentModal = () => {
-  return <div>DeleteCommentModal</div>;
+export const DeleteCommentModal = ({ closeModal }) => {
+  return (
+    <div className="modal__overlay">
+      <div className="modal__content">
+        <div className="modal__title">Delete comment</div>
+        <p className="modal__text">
+          Are you sure you want to delete this comment? This will remove the comment and can't be undone.
+        </p>
+        <div className="modal__flex-buttons">
+          <Button onClick={closeModal} className={'modal__button modal__button-gray'} type={'box-button'}>
+            NO, CANCEL
+          </Button>
+          <Button className={'modal__button modal__button-red'} type={'box-button'}>
+            YES, DELETE
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
