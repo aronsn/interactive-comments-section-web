352291d3704eeb95791612d0a9969c0d59362eb9
Aron Snekkestad <aron@snekkestad.com>
2024-06-10 22:27:02 +0200

    feat: create a fetch util function that takes url, method, and body as parameter and creates a http request

---
+++ src/utils/fetchUtil.js
@@ -0,0 +1,27 @@
+
+export const fetchUtil = async (url, method, body) => {
+    try {
+        const options = {
+            method: method,
+            credentials: "omit",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
+
+        if (body) {
+            options.body = JSON.stringify(body);
+        }
+
+        const response = await fetch(url, options);
+
+        if (!response.ok) {
+            throw new Error(`Response not ok: ${response.status}`)
+        }
+
+        return response.json();
+
+    } catch (error) {
+        throw new Error('Unable to make the request to the server\n', error)
+    }
+}
