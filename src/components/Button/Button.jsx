7105fc9ed065758190b78d2fa07407ae7d0dd059
Aron Snekkestad <aron@snekkestad.com>
2024-11-02 22:50:59 +0100

    reformat: remove whitespace

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,8 +3,9 @@ import './Button.css';
 
 export function Button({ children, className, iconSvg, onClick, type }) {
   const checkType = type === 'icon-button' ? 'icon' : null || type === 'box-button' ? 'box' : null;
+
   return (
-    <button onClick={onClick ? onClick : null} className={`${checkType} ${className ? className : ''}`}>
+    <button onClick={onClick} className={`${checkType} ${className ? className : ''}`}>
       {iconSvg ? iconSvg : null}
       {children}
     </button>
