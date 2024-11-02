bffb93d157575472213834b4e23496da08730ed8
Aron Snekkestad <aron@snekkestad.com>
2024-11-02 22:53:14 +0100

    feat: rework modal logic for delete and provide props. And create update comment functin api call

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -5,6 +5,9 @@ import { replySvg, editSvg, deleteSvg } from '../../utils/exportAssets';
 import './Comment.css';
 import { DeleteCommentModal } from '../DeleteCommentModal/DeleteCommentModal';
 import { formatDate } from '../../utils/format';
+import Feedback from '../Feedback/Feedback';
+import { useFetchCommentsContext } from '../../utils/CommentsProvider';
+import { fetchUtil } from '../../utils/fetchUtil';
 
 export function Comment({
   id,
@@ -16,13 +19,23 @@ export function Comment({
   createdAt,
   comment,
   score,
-  setModal,
-  modal,
-  filterComment,
 }) {
+  const { fetchComments } = useFetchCommentsContext();
+
   const [display, setDisplay] = useState(false);
   const [edit, setEdit] = useState(false);
-  const [input, setInput] = useState(`@${replyingTo}, ${comment}`);
+  const [input, setInput] = useState(`${comment}`);
+  const [modal, setModal] = useState(false);
+
+  const handleUpdateComment = () => {
+    const body = {
+      id: id,
+      newContent: input,
+      username: username,
+    };
+    fetchUtil('PATCH', body).then(() => fetchComments());
+    setEdit((prevState) => !prevState);
+  };
 
   return (
     <div className="comment">
@@ -47,23 +60,16 @@ export function Comment({
             {comment}
           </p>
         )}
-        <div className="card__feedback">
-          <button className="card__feedback-button ">
-            <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
-              <path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" />
-            </svg>
-          </button>
-          <div className="card__feedback-number card--color-blue">{score}</div>
-          <button className="card__feedback-button">
-            <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg">
-              <path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" />
-            </svg>
-          </button>
-        </div>
+        <Feedback id={id} score={score} />
         <div className="card__button-group">
           {isCurrentUser ? (
             <>
-              <Button onClick={setModal} className="card__delete" type={'icon-button'} iconSvg={deleteSvg}>
+              <Button
+                onClick={() => setModal((prevState) => !prevState)}
+                className="card__delete"
+                type={'icon-button'}
+                iconSvg={deleteSvg}
+              >
                 Delete
               </Button>
               <Button
@@ -88,21 +94,22 @@ export function Comment({
           )}
         </div>
         {edit ? (
-          <Button className={'card__update'} type={'box-button'}>
+          <Button className={'card__update'} type={'box-button'} onClick={handleUpdateComment}>
             UPDATE
           </Button>
         ) : null}
       </section>
       {display ? (
-        <CommentInput currentUserImage={currentUserImage} replyingTo={username} placeholder={'REPLY'} />
+        <CommentInput
+          setDisplay={() => setDisplay((prevState) => !prevState)}
+          currentUserImage={currentUserImage}
+          replyingTo={username}
+          id={id}
+          placeholder={'REPLY'}
+        />
       ) : null}
       {modal ? (
-        <DeleteCommentModal
-          closeModal={setModal}
-          replyingTo={replyingTo ? true : false}
-          filterComment={filterComment}
-          id={id}
-        />
+        <DeleteCommentModal closeModal={() => setModal((prevState) => !prevState)} username={username} id={id} />
       ) : null}
     </div>
   );
